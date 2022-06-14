2aef65e099d444ee1f4e302aa46008f389e99de6
Alex <lex.courtney18@gmail.com>
2022-06-14 18:41:29 -0500

    add event listener to capture button click

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var startingPageEl = document.querySelector("#starting-page");
+
+var startQuiz = function (event) {
+    console.log("hello");
+}
+
+
+
+
+startingPageEl.addEventListener("click", startQuiz);
